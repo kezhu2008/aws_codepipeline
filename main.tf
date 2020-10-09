8a62da81fc1dcc5cf03689801637a8940986fdf4
Ke <kezhu2007@gmail.com>
2020-10-09 20:16:49 +1100

    re

--- main.tf
+++ main.tf
@@ -148,7 +148,8 @@ resource "aws_iam_role_policy" "pipeline_policy" {
   name = "pipeline_policy"
   role = aws_iam_role.codepipeline_role.id
 
-  policy = <<EOF{
+  policy = <<EOF
+{
     "Version": "2012-10-17",
     "Statement": [
       {
