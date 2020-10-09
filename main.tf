be662a8f6388c595b4e07f2262c43b98f70329ff
Ke <kezhu2007@gmail.com>
2020-10-09 14:42:08 +1100

    Add to repository

---
+++ main.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  version = "2.33.0"
+
+  region = var.aws_region
+}
+
+
+resource "aws_codepipeline" "codepipeline" {
+  name     = var.pipeline_name
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+
+  }
+
+  stage {
+    name = "CodeCommit"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+
+      output_artifacts = ["source_output"]
+
+      RepositoryName = {
+        Repo       = "test_code_commit"
+        Branch     = "master"
+        PollForSourceChanges = false
+      }
+    }
+  }
+}
+
+
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = var.pipeline_role_name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = var.pipeline_role_policy_name
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.codepipeline_bucket.arn}",
+        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type  = string
+  default = "ap_southeast_2"
+}
+
+variable "pipeline_name" {
+  type  = string
+  default = "default_pipeline"
+}
+
+variable "pipeline_s3_bucket" {
+  type  = string
+  default = "s3_coderepo"
+}
+
+variable "pipeline_role_name" {
+  type  = string
+  default = "default_pipeline_role"
+}
