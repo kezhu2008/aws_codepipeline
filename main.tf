232a3621f423cf0c820765a2d64e0773db781603
Ke <kezhu2007@gmail.com>
2020-10-09 16:28:08 +1100

    test

--- main.tf
+++ main.tf
@@ -118,5 +118,21 @@ EOF
 resource "aws_iam_role" "codepipeline_role" {
   name = var.pipeline_role_name
 
-  assume_role_policy = data.template_file.pipeline_policy.rendered
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "codepipeline.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+  }
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_role-attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
 }
