106870dafe53b550da8c5a40931266d441ace419
Ke <kezhu2007@gmail.com>
2020-10-09 15:14:56 +1100

    changed name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_codebuild_project" "codebuild_project" {
-  name = var.codebuild_project.name
+  name = var.codebuild_project_name
   artifacts = {
     type = "NO_ARTIFACTS"
   }
