2100debe49d02af93622233d983452e0f337a347
Ke <kezhu2007@gmail.com>
2020-10-09 16:45:47 +1100

    aa

--- main.tf
+++ main.tf
@@ -115,6 +115,9 @@ resource "aws_iam_role" "codebuild_role" {
 EOF
 }
 
+
+
+
 resource "aws_iam_role" "codepipeline_role" {
   name = var.pipeline_role_name
 
@@ -128,6 +131,25 @@ resource "aws_iam_role" "codepipeline_role" {
           "Service": "codepipeline.amazonaws.com"
         },
         "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_iam_role_policy" "pipeline_policy" {
+  name = "pipeline_policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Stmt1602222091816",
+        "Action": "s3:*",
+        "Effect": "Allow",
+        "Resource": "${aws_s3_bucket.codepipeline_bucket.arn}/*"
       }
     ]
   }
